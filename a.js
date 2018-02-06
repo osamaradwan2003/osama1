6bf552f8d29fc895a46f4cdd5283a59f56d42cdf
Osamaradwan523 <36177671+Osamaradwan523@users.noreply.github.com>
2018-02-06 03:18:16 +0200

    Add files via upload

---
+++ a.js
@@ -0,0 +1,84 @@
+/*global $, window, */
+
+$(window).ready(function () {
+ 
+    "use strict";
+    
+    var url_blog = "http://www.arabic-designer.tk",
+        num_post = 10;
+    
+    var ajaxOPtion = {
+        
+        url: '' + url_blog + "/feeds/posts/default?alt=json-in-script&max-results=" + num_post + '',
+        dataType: "jsonp",
+        type: "get",
+        success: function (data) {
+        
+            var p,
+                s = '',
+                pt,
+                entry = data.feed.entry;
+            if (entry !== undefined) {
+                
+                s = "<ul>";
+                
+                for (var i = 0; i < entry.length; i++) {
+                     
+                     for (var j = 0; j < entry[i].link.length; j++)
+                        
+                         if (entry[i].link[j].rel == "alternate"){
+                             
+                             p = entry[i].link[j].href; break;
+                             
+                             
+                         }
+                            
+                            pt = entry[i].title.$t;
+                    
+                    s += '<li><a href="' + p + '" target="_blank">' + pt + '</a></li>'
+                            
+                    
+                     }
+                
+                    s += "</ul>";
+                
+                    $("#recentpostbreaking").html(s);
+                
+                function t () {
+                    
+                    $("#recentpostbreaking li:first").slideUp(function () {
+                        
+                        $("#recentpostbreaking ul").slideDown();
+                        
+                        
+                    })
+                    
+                    
+                }
+                
+                
+                setInterval(function(){ t() }, 5000);
+                
+                
+                
+            } else {
+                
+                $('#recentpostbreaking').html('<span>No result!</span>');
+            }
+        
+        
+        
+        },error: function () {
+            
+            $('#recentpostbreaking').html('<strong>Error Loading Feed!</strong>');
+        }
+        
+        
+    }
+    
+    
+    $.ajax(ajaxOPtion);
+    
+    
+    
+});
